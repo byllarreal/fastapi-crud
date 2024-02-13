40512a59a4c8a6c079d45db7f47d0b94aaf21bc9
ChocoPlot <52388306+EdwarMontano@users.noreply.github.com>
2024-02-13 14:29:38 -0500

    Update database.py
    revisar la documentación de FastApi y sqlalchemy 
    
    https://fastapi.tiangolo.com/tutorial/sql-databases/#note
    
    ```
    connect_args={"check_same_thread": False}
    ```
    ...sólo es necesario para SQLite. No es necesario para otras bases de datos.

--- app/database.py
+++ app/database.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@host.docker.internal:5432/postgres"
 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
